6ee8184fcfa57342ced5835e25bb090d13d1835f
ketjj <jorjolianiket@gmail.com>
2022-05-21 16:07:39 +0200

    adding dynamic content with query already inserted

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
   <HeaderComp />
-  <MainComp />
+  <MainComp :filmlist="movieLists"/>
   </div>
 </template>
 
@@ -9,12 +9,38 @@
 import MainComp from './components/MainComp'
 import HeaderComp from './components/HeaderComp'
 
+import axios from 'axios'
+
 
 export default {
   name: 'App',
   components: {
    MainComp,
    HeaderComp
+  },
+   data(){
+     return{
+       apiUrl: 'https://api.themoviedb.org/3/search/movie',
+       apiParametres:{
+         api_key: 'db434741c3c9df5b6b054b9b08d39df2',
+         language: 'it_IT',
+         query: 'matrix'
+       },
+       movieLists: [],
+     }
+   },
+  mounted(){
+    this.getApi()
+  },
+  methods: {
+    getApi(){
+      axios.get(this.apiUrl, {params: this.apiParametres})
+      .then(res =>{
+        console.log(res.data.results, '------------')
+        this.movieLists = res.data.results;
+        
+      })
+    }
   }
 }
 </script>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,17 +1,26 @@
 <template>
-  <div>MAIN
+    
+  <div>
+    <MainCard
+    v-for="(singleFilm,index) in filmlist" :key="`film${index}`"
+    :film="singleFilm"
+    />
 
   </div>
 </template>
 
 <script>
+import MainCard from '@/components/MainCard'
 
 export default {
   name: 'MainComp',
   components:{
+    MainCard
+  },
 
+  props:{
+    filmlist:Array
   }
-
 }
 </script>
 
